05c9fa34d9fb37f38668025f1c179bf0a4abf6fa
SibrenBD <302454429@student.rocmondriaan.nl>
2023-09-06 10:15:57 +0200

    nieuw 6-9

--- HG_LG.js
+++ HG_LG.js
@@ -2,3 +2,7 @@ function myFunction() {
     document.getElementById("Intecator").innerHTML = "Hello World";
   }
 
+  const higherButton = document.querySelector('.higher_btn_1');
+
+  console.log("higherButton is " + higherButton);
+
