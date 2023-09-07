f7c0fb19b2ab2f7a1262537ca270fcd1e07dc439
MikevdVeer <302815715@student.rocmondriaan.nl>
2023-09-07 10:25:07 +0200

    edit

--- HG_LG.js
+++ HG_LG.js
@@ -2,7 +2,5 @@ function myFunction() {
     document.getElementById("Intecator").innerHTML = "Hello World";
   }
 
-  const higherButton = document.querySelector('.higher_btn_1');
 
-  console.log("higherButton is " + higherButton);
 
