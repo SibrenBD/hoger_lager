02b0c4564b3ec8fcf54acd5d803c0b9de389b5a4
SibrenBD <302454429@student.rocmondriaan.nl>
2023-09-07 15:19:09 +0200

    random nummers

--- HG_LG.js
+++ HG_LG.js
@@ -1,3 +1,19 @@
+
+
+
+let nummer1 = Math.floor(Math.random() * 11);
+let nummer2 = Math.floor(Math.random() * 11)
+
+console.log(nummer1)
+console.log(nummer2)
+
+
+
+
+
+
+
+
 function myFunction() {
     document.getElementById("Intecator").innerHTML = "Hello World";
   }
