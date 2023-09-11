c2feaee671ac8aa310d6c2aa9590f91d1b2b5e75
SibrenBD <302454429@student.rocmondriaan.nl>
2023-09-11 11:30:03 +0200

    function

--- HG_LG.js
+++ HG_LG.js
@@ -1,10 +1,17 @@
 
 
 
+
+
+
 let nummer1 = Math.round(Math.random() * 6) + 1;
 let nummer2 = Math.round(Math.random() * 6) + 1;
+ 
+const dobbel_p_1 = document.querySelector(".dobbel_1")
 
-console.log(nummer1)
+
+
+console.log(nummer1);
 console.log(nummer2)
 
 
@@ -12,11 +19,10 @@ console.log(nummer2)
 
 
 
+function gooi() {
+  dobbel_p_1.src = "/images/dobbelsteen2.png"
+}
 
-
-function myFunction() {
-    document.getElementById("Intecator").innerHTML = "Hello World";
-  }
 
 
 
