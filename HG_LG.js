97f855c2767186b98ddb644da1d3092977b86e5a
SibrenBD <302454429@student.rocmondriaan.nl>
2023-09-11 11:16:48 +0200

    update random

--- HG_LG.js
+++ HG_LG.js
@@ -1,8 +1,8 @@
 
 
 
-let nummer1 = Math.round(Math.random() * 6);
-let nummer2 = Math.round(Math.random() * 6);
+let nummer1 = Math.round(Math.random() * 6) + 1;
+let nummer2 = Math.round(Math.random() * 6) + 1;
 
 console.log(nummer1)
 console.log(nummer2)
