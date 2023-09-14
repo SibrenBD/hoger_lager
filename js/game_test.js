93263599a1f570bddd46fa0537cce3a802434c34
MikevdVeer <302815715@student.rocmondriaan.nl>
2023-09-14 13:39:42 +0200

    asd

--- js/game_test.js
+++
@@ -1,9 +0,0 @@
-
-function play() {
-    document.querySelector(".result").innerHTML = "hallo"
-}
-function img_switch() {
-    const image = document.querySelector(".dobbelsteen")
-    image.src = "/images/Dobbelsteen2.png"
-
-}
