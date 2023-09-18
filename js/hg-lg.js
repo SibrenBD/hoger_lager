fc9634ad251f336b6c73053b90ecaf05963cf8a9
SibrenBD <302454429@student.rocmondriaan.nl>
2023-09-18 09:44:07 +0200

    snek case

---
+++ js/hg-lg.js
@@ -0,0 +1,125 @@
+
+
+
+
+const dobbelp1 = document.querySelector(".dobbel1");
+// computer score aanpassen naar autoselector
+
+let cpRoll
+let plRoll
+
+
+let plScore = 5;
+
+
+let plBet
+
+function hoger() {
+  plBet = "hoger";
+  document.querySelector(".plBet").innerHTML = "player bet: " + plBet;
+}
+function lager() {
+  plBet = "lager";
+  document.querySelector(".plBet").innerHTML = "player bet: " + plBet;
+}
+
+
+
+// start van de site
+
+let ronde = false;
+
+if (ronde == false) {
+  cpRoll = Math.floor(Math.random() * 6) + 1;
+
+  switch (cpRoll) {
+    case 1:
+      dobbelp1.src = "/images/Dobbelsteen1.png"
+      break
+    case 2:
+      dobbelp1.src = "/images/Dobbelsteen2.png"
+      break
+    case 3:
+      dobbelp1.src = "/images/Dobbelsteen3.png"
+      break
+    case 4:
+      dobbelp1.src = "/images/Dobbelsteen4.png"
+      break
+    case 5:
+      dobbelp1.src = "/images/Dobbelsteen5.png"
+      break
+    case 6:
+      dobbelp1.src = "/images/Dobbelsteen6.png"
+  }
+}
+
+document.querySelector(".cpRoll").innerHTML = "computer roll: " + cpRoll;
+document.querySelector(".plRoll").innerHTML = "player roll: " + plRoll;
+
+
+
+
+
+
+
+
+function gooi() {
+  if (ronde == false) {
+    
+    ronde = true
+
+    if (plBet != undefined) {
+      plRoll = Math.floor(Math.random() * 6) + 1;
+
+      switch (plRoll) {
+        case 1:
+          dobbelp1.src = "/images/Dobbelsteen1.png"
+          break
+        case 2:
+          dobbelp1.src = "/images/Dobbelsteen2.png"
+          break
+        case 3:
+          dobbelp1.src = "/images/Dobbelsteen3.png"
+          break
+        case 4:
+          dobbelp1.src = "/images/Dobbelsteen4.png"
+          break
+        case 5:
+          dobbelp1.src = "/images/Dobbelsteen5.png"
+          break
+        case 6:
+          dobbelp1.src = "/images/Dobbelsteen6.png"
+      }
+
+      document.querySelector(".plRoll").innerHTML = "player roll: " + plRoll;
+      document.querySelector(".plBet").innerHTML = "player bet: " + plBet;
+    }
+  
+
+
+
+
+
+
+  }
+  else {
+    ronde = false
+  }
+}
+
+
+
+
+
+
+
+
+
+// overlay
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
