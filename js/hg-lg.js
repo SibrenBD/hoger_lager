c6314df492fea9cb181a163d27485378a460b4be
SibrenBD <302454429@student.rocmondriaan.nl>
2023-09-22 11:58:09 +0200

    no gooi button

--- js/hg-lg.js
+++ js/hg-lg.js
@@ -17,39 +17,6 @@ let plBet
 function hoger() {
   plBet = "hoger";
   document.querySelector(".plBet").innerHTML = "player bet: " + plBet;
-}
-function lager() {
-  plBet = "lager";
-  document.querySelector(".plBet").innerHTML = "player bet: " + plBet;
-}
-
-
-
-// start van de site
-
-let ronde = false;
-
-
-cpRoll = Math.floor(Math.random() * 6) + 1;
-
-
-
-
-document.querySelector(".cpRoll").innerHTML += cpRoll;
-document.querySelector(".plRoll").innerHTML = "player roll: " + plRoll;
-
-
-  document.querySelector(".beurt").innerHTML = "Druk op gooien om het spel te starten"
-
-
-  
-
-
-
-
-
-
-function gooi() {
 
   if (ronde == false) {
     
@@ -78,7 +45,78 @@ function gooi() {
 
     document.querySelector(".cpRoll").innerHTML = "computer roll: " + cpRoll;
     
-    document.querySelector(".beurt").innerHTML = "beurt: computer"
+    document.querySelector(".turn").innerHTML = "beurt: computer"
+
+    ronde = true
+
+  }
+  else if (ronde == true) {
+    
+
+    plRoll = Math.floor(Math.random() * 6) + 1;
+
+    switch (plRoll) {
+      case 1:
+        dobbelp1.src = "/images/Dobbelsteen1.png"
+        break
+      case 2:
+        dobbelp1.src = "/images/Dobbelsteen2.png"
+        break
+      case 3:
+        dobbelp1.src = "/images/Dobbelsteen3.png"
+        break
+      case 4:
+        dobbelp1.src = "/images/Dobbelsteen4.png"
+        break
+      case 5:
+        dobbelp1.src = "/images/Dobbelsteen5.png"
+        break
+      case 6:
+        dobbelp1.src = "/images/Dobbelsteen6.png"
+    }
+
+    document.querySelector(".plRoll").innerHTML = "player roll: " + plRoll;
+
+    document.querySelector(".turn").innerHTML = "beurt: player"
+
+    ronde = false
+
+  }
+  
+}
+
+function lager() {
+  plBet = "lager";
+  document.querySelector(".plBet").innerHTML = "player bet: " + plBet;
+
+  if (ronde == false) {
+    
+
+    cpRoll = Math.floor(Math.random() * 6) + 1;
+
+    switch (cpRoll) {
+      case 1:
+        dobbelp1.src = "/images/Dobbelsteen1.png"
+        break
+      case 2:
+        dobbelp1.src = "/images/Dobbelsteen2.png"
+        break
+      case 3:
+        dobbelp1.src = "/images/Dobbelsteen3.png"
+        break
+      case 4:
+        dobbelp1.src = "/images/Dobbelsteen4.png"
+        break
+      case 5:
+        dobbelp1.src = "/images/Dobbelsteen5.png"
+        break
+      case 6:
+        dobbelp1.src = "/images/Dobbelsteen6.png"
+    }
+
+    document.querySelector(".cpRoll").innerHTML = "computer roll: " + cpRoll;
+    
+    document.querySelector(".turn").innerHTML = "beurt: computer"
 
     ronde = true
 
@@ -112,16 +150,36 @@ function gooi() {
 
     document.querySelector(".plRoll").innerHTML = "player roll: " + plRoll;
 
-    document.querySelector(".beurt").innerHTML = "beurt: player"
+    document.querySelector(".turn").innerHTML = "beurt: player"
 
     ronde = false
 
   }
+}
+
+
+
+// start van de site
+
+let ronde = false;
+
+
+cpRoll = Math.floor(Math.random() * 6) + 1;
+
+
+
+
+document.querySelector(".cpRoll").innerHTML += cpRoll;
+document.querySelector(".plRoll").innerHTML = "player roll: " + plRoll;
+
+
+  document.querySelector(".turn").innerHTML = "Druk op gooien om het spel te starten"
+
 
   
 
 
-}
+
 
 
 
