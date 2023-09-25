5b993e4831ec45b49bb674a7802b5ee0898e0bf0
SibrenBD <302454429@student.rocmondriaan.nl>
2023-09-25 11:32:32 +0200

    start knop en nieuwe turn switch functie

--- js/hg-lg.js
+++ js/hg-lg.js
@@ -16,12 +16,100 @@ document.querySelector(".score").innerHTML = "score: " + score
 
 // start van de site
 
-let ronde = false;
+let ronde;
 
 document.querySelector(".cpRoll").innerHTML += cpRoll;
 document.querySelector(".plRoll").innerHTML = "player roll: " + plRoll;
-document.querySelector(".turn").innerHTML = "Druk op gooien om het spel te starten"
+document.querySelector(".turn").innerHTML = "Druk op 'start game' om het spel te starten"
 
+
+
+
+// ronde wisseling
+
+function turnSwitch() {
+
+  if (ronde == false) {
+
+    cpRoll = Math.floor(Math.random() * 6) + 1;
+
+    switch (cpRoll) {
+      case 1:
+        dobbelp1.src = "/images/1.png"
+        break
+      case 2:
+        dobbelp1.src = "/images/2.png"
+        break
+      case 3:
+        dobbelp1.src = "/images/3.png"
+        break
+      case 4:
+        dobbelp1.src = "/images/4.png"
+        break
+      case 5:
+        dobbelp1.src = "/images/5.png"
+        break
+      case 6:
+        dobbelp1.src = "/images/6.png"
+    }
+
+    document.querySelector(".cpRoll").innerHTML = "computer roll: " + cpRoll;
+    
+    document.querySelector(".turn").innerHTML = "beurt: computer"
+
+    ronde = true
+
+  }
+  else if (ronde == true) {
+    
+
+    plRoll = Math.floor(Math.random() * 6) + 1;
+
+    if (plRoll > cpRoll) {
+      score += 1;
+    }
+    else if (plRoll < cpRoll){
+      score -= 1;
+    }
+    
+    document.querySelector(".score").innerHTML = "score: " + score
+
+    switch (plRoll) {
+      case 1:
+        dobbelp1.src = "/images/1.png"
+        break
+      case 2:
+        dobbelp1.src = "/images/2.png"
+        break
+      case 3:
+        dobbelp1.src = "/images/3.png"
+        break
+      case 4:
+        dobbelp1.src = "/images/4.png"
+        break
+      case 5:
+        dobbelp1.src = "/images/5.png"
+        break
+      case 6:
+        dobbelp1.src = "/images/6.png"
+    }
+
+    document.querySelector(".plRoll").innerHTML = "player roll: " + plRoll;
+
+    document.querySelector(".turn").innerHTML = "beurt: player"
+
+    ronde = false
+
+  }
+}
+
+
+function startGame() {
+  document.querySelector(".btn-start").remove();
+  ronde = false
+}
+
+// knoppen hoger en lager
 
 function hoger() {
   plBet = "hoger";
