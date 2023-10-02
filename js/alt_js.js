3fb014dc9a051b99ad0d9ab1cb33267a87533905
MikevdVeer <302815715@student.rocmondriaan.nl>
2023-10-02 12:16:11 +0200

    Html, Css finshed

--- js/alt_js.js
+++ js/alt_js.js
@@ -1,1 +1,45 @@
+// overlay
 
+
+const rulesBtn = document.querySelector(".rules");
+rulesBtn.addEventListener("click", on);
+
+const overlay = document.querySelector(".overlay");
+overlay.addEventListener("click", off);
+
+function on() {
+  document.querySelector(".overlay").style.display = "block";
+}
+
+function off() {
+  document.querySelector(".overlay").style.display = "none";
+}
+
+// knoppen
+
+const hogerBtn = document.querySelector(".btn-hoger");
+hogerbtn.addEventListener("click", higher);
+
+const lagerBtn = document.querySelector(".btn-lager");
+lagerBtn.addEventListener("click", lower)
+
+const startBtn = document.querySelector(".btn-start");
+startBtn.addEventListener("click", startGame);
+
+
+function higher() {
+    
+}
+
+function lower() {
+
+}
+
+function start() {
+
+}
+
+let cpRoll = "niks";
+let plRoll = "niks";
+let plBet;
+let score = 10;
