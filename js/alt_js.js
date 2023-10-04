e71aea1ed25e30f1c5d4bfde1c35d66f23e5ff1d
SibrenBD <302454429@student.rocmondriaan.nl>
2023-10-04 16:21:25 +0200

    game werkt

--- js/alt_js.js
+++ js/alt_js.js
@@ -20,67 +20,128 @@ const dice = document.querySelector(".dice");
 
 // knoppen
 
-const hogerBtn = document.querySelector(".btn-hoger");
-hogerBtn.addEventListener("click", higher);
+const higherBtn = document.querySelector(".btn-higher");
+higherBtn.addEventListener("click", higher);
 
-const lagerBtn = document.querySelector(".btn-lager");
-lagerBtn.addEventListener("click", lower)
+const lowerBtn = document.querySelector(".btn-lower");
+lowerBtn.addEventListener("click", lower);
 
 const startBtn = document.querySelector(".btn-start");
 startBtn.addEventListener("click", start);
 
+// score display
+
+const scoreDisplay = document.querySelector(".score");
+
+function displayScore() {
+  scoreDisplay.textContent = "score: ";
+  scoreDisplay.textContent += score;
+}
+
+// game 
+
 
 let gameStarted = false;
-
 let currentNumber;
-let plBet;
+let prevNumber;
+let playerBet;
 let score = 10;
+
+displayScore();
 
 function higher() {
   if (gameStarted == true) {
-    hogerBtn.setAttribute("disabled", "disabled");
-    lagerBtn.removeAttribute("disabled", "disabled");
+    higherBtn.setAttribute("disabled", "disabled");
+    lowerBtn.removeAttribute("disabled", "disabled");
+    startBtn.removeAttribute("disabled", "disabled");
+    playerBet = "higher";
   }
 }
 
 function lower() {
   if (gameStarted == true) {
-
-    lagerBtn.setAttribute("disabled", "disabled");
-    hogerBtn.removeAttribute("disabled", "disabled");
+    lowerBtn.setAttribute("disabled", "disabled");
+    higherBtn.removeAttribute("disabled", "disabled");
+    startBtn.removeAttribute("disabled", "disabled");
+    playerBet = "lower";
   }
 }
 
 function start() {
   if (gameStarted == false) {
-    currentNumber = Math.floor(Math.random() * 6) + 1
-    console.log(currentNumber);
     gameStarted = true;
     startBtn.textContent = "go!";
+    startBtn.setAttribute("disabled", "disabled");
+    currentNumber = Math.floor(Math.random() * 6) + 1;
+    imgChange();
   }
   else if (gameStarted == true) {
-    currentNumber = Math.floor(Math.random() * 6) + 1
-    console.log(currentNumber);
     submit();
+    imgChange();
   }
 }
 
 function submit() {
-  lagerBtn.removeAttribute("disabled", "disabled");
-  hogerBtn.removeAttribute("disabled", "disabled");
+  lowerBtn.removeAttribute("disabled", "disabled");
+  higherBtn.removeAttribute("disabled", "disabled");
+  startBtn.setAttribute("disabled", "disabled");
+  prevNumber = currentNumber;
+  currentNumber = Math.floor(Math.random() * 6) + 1;
+
+
+  switch (playerBet) {
+    case "higher":
+      if (currentNumber > prevNumber) {
+        console.log("Lesss goo");
+      }
+      else if (currentNumber < prevNumber) {
+        console.log("Aww damn");
+        score--;
+        displayScore();
+      }
+      else if (currentNumber == prevNumber) {
+        console.log("bruh");
+      }
+      break;
+    case "lower":
+      if (currentNumber < prevNumber) {
+        console.log("Lesss goo");
+      }
+      else if (currentNumber > prevNumber) {
+        console.log("Aww damn");
+        score--;
+        displayScore();
+      }
+      else if (currentNumber == prevNumber) {
+        console.log("bruh");
+      }
+      break;
+  }
+  playerBet = "nothing";
 }
+
+
 
 function imgChange() {
   switch (currentNumber) {
     case 1:
-      dice.src = "images/1.png"
+      dice.src = "/images/1.png";
       break;
     case 2:
-      dice.src = "images/2.png"
+      dice.src = "/images/2.png";
+      break;
     case 3:
-      dice.src = "images/1.png"
+      dice.src = "/images/3.png";
       break;
     case 4:
-      dice.src = "images/2.png"
+      dice.src = "/images/4.png";
+      break;
+    case 5:
+      dice.src = "/images/5.png";
+      break;
+    case 6:
+      dice.src = "/images/6.png";
+      break;
   }
 }
+
