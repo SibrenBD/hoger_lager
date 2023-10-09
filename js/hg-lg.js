6d2415734c79bdd8ed9286b70b21dbaeb1956760
MikevdVeer <302815715@student.rocmondriaan.nl>
2023-10-09 22:03:13 +0200

    map update

--- js/hg-lg.js
+++ js/hg-lg.js
@@ -1,137 +1,10 @@
+// overlay spelregels
+const rulesBtn = document.querySelector(".rules");
+rulesBtn.addEventListener("click", on);
 
-const dobbelp1 = document.querySelector(".dobbel1");
-// computer score aanpassen naar autoselector
+const overlay = document.querySelector(".overlay");
+overlay.addEventListener("click", off);
 
-let cpRoll = "niks";
-let plRoll = "niks";
-let plBet;
-let score = 10;
-
-let time;
-
-
-// image
-
-
-
-// start van de site
-
-let ronde;
-
-document.querySelector(".cpRoll").innerHTML += cpRoll;
-document.querySelector(".plRoll").innerHTML = "player roll: " + plRoll;
-document.querySelector(".turn").innerHTML = "Druk op 'start game' om het spel te starten"
-document.querySelector(".score").innerHTML = "score: " + score
-
-// ronde wisseling
-
-function turnSwitch() {
-
-  if (ronde == false) {
-
-    cpRoll = Math.floor(Math.random() * 6) + 1;
-
-    switch (cpRoll) {
-      case 1:
-        dobbelp1.src = "/images/1.png"
-        break
-      case 2:
-        dobbelp1.src = "/images/2.png"
-        break
-      case 3:
-        dobbelp1.src = "/images/3.png"
-        break
-      case 4:
-        dobbelp1.src = "/images/4.png"
-        break
-      case 5:
-        dobbelp1.src = "/images/5.png"
-        break
-      case 6:
-        dobbelp1.src = "/images/6.png"
-    }
-
-    document.querySelector(".cpRoll").innerHTML = "computer roll: " + cpRoll;
-    
-    document.querySelector(".turn").innerHTML = "beurt: computer"
-
-    ronde = true
-
-  }
-  else if (ronde == true) {
-    
-
-    plRoll = Math.floor(Math.random() * 6) + 1;
-
-    if (plRoll > cpRoll) {
-      score += 1;
-    }
-    else if (plRoll < cpRoll){
-      score -= 1;
-    }
-    
-    document.querySelector(".score").innerHTML = "score: " + score
-
-    switch (plRoll) {
-      case 1:
-        dobbelp1.src = "/images/1.png"
-        break
-      case 2:
-        dobbelp1.src = "/images/2.png"
-        break
-      case 3:
-        dobbelp1.src = "/images/3.png"
-        break
-      case 4:
-        dobbelp1.src = "/images/4.png"
-        break
-      case 5:
-        dobbelp1.src = "/images/5.png"
-        break
-      case 6:
-        dobbelp1.src = "/images/6.png"
-    }
-
-    document.querySelector(".plRoll").innerHTML = "player roll: " + plRoll;
-
-    document.querySelector(".turn").innerHTML = "beurt: player"
-
-    
-    ronde = false
-
-  }
-}
-
-// knoppen hoger en lager
-
-function hoger() {
-  hogerBtn.setAttribute("disabled", "disabled");
-  if (ronde != undefined) {
-    plBet = "hoger";
-
-    turnSwitch()
-  }
-
-}
-
-function lager() {
-  
-  if (ronde != undefined) {
-    plBet = "lager";
-    
-    turnSwitch()
-  }
-
-}
-
-function startGame() {
-  document.querySelector(".btn-start").remove();
-  ronde = false
-
-  turnSwitch()
-}
-
-// overlay
 function on() {
   document.querySelector(".overlay").style.display = "block";
 }
@@ -139,3 +12,177 @@ function on() {
 function off() {
   document.querySelector(".overlay").style.display = "none";
 }
+
+// img
+const dice = document.querySelector(".dice");
+
+// buttons
+
+const higherBtn = document.querySelector(".btn-higher");
+higherBtn.addEventListener("click", higher);
+
+const lowerBtn = document.querySelector(".btn-lower");
+lowerBtn.addEventListener("click", lower);
+
+const startBtn = document.querySelector(".btn-start");
+startBtn.addEventListener("click", start);
+
+// Win or Lose
+let playerWon;
+const won = document.querySelector(".won");
+const draw = document.querySelector(".draw")
+const lose = document.querySelector(".lose");
+
+
+function showResult() {
+  switch (playerWon) {
+    case true:
+      won.textContent = "Je hebt gewonnen";
+      break;
+    case false:
+      lose.textContent = "Je hebt verloren";
+      break;
+    case undefined:
+      draw.textContent = "Gelijk spel"
+  }
+}
+
+function hideResults() {
+  won.textContent = "";
+  lose.textContent = "";
+  draw.textContent = ""
+}
+
+// score display
+const scoreDisplay = document.querySelector(".score");
+
+function displayScore() {
+  scoreDisplay.textContent = "score: ";
+  scoreDisplay.textContent += score;
+}
+
+// sound script
+const audio = document.querySelector(".audio");
+function playSound() {
+  audio.play();
+}
+
+// game 
+let gameStarted = false;
+let currentNumber;
+let prevNumber;
+let playerBet;
+let score = 10;
+
+displayScore();
+
+function higher() {
+  hideResults();
+  if (gameStarted == true) {
+    higherBtn.setAttribute("disabled", "disabled");
+    lowerBtn.removeAttribute("disabled", "disabled");
+    startBtn.removeAttribute("disabled", "disabled");
+    playerBet = "higher";
+  }
+}
+
+function lower() {
+  hideResults();
+  if (gameStarted == true) {
+    lowerBtn.setAttribute("disabled", "disabled");
+    higherBtn.removeAttribute("disabled", "disabled");
+    startBtn.removeAttribute("disabled", "disabled");
+    playerBet = "lower";
+  }
+}
+
+function start() {
+  if (gameStarted == false) {
+    gameStarted = true;
+    startBtn.textContent = "Submit";
+    startBtn.setAttribute("disabled", "disabled");
+    currentNumber = Math.floor(Math.random() * 6) + 1;
+    imgChange();
+  }
+  else if (gameStarted == true) {
+    submit();
+    imgChange();
+    playSound();
+  }
+}
+
+function submit() {
+  lowerBtn.removeAttribute("disabled", "disabled");
+  higherBtn.removeAttribute("disabled", "disabled");
+  startBtn.setAttribute("disabled", "disabled");
+  prevNumber = currentNumber;
+  currentNumber = Math.floor(Math.random() * 6) + 1;
+
+// THE CHECK
+
+  switch (playerBet) {
+    case "higher":
+      if (currentNumber > prevNumber) {
+        console.log("Lesss goo");
+        playerWon = true;
+        showResult();
+      }
+      else if (currentNumber < prevNumber) {
+        console.log("Aww damn");
+        playerWon = false;
+        showResult();
+        score--;
+        displayScore();
+      }
+      else if (currentNumber == prevNumber) {
+        console.log("bruh");
+        playerWon = undefined;
+        showResult();
+      }
+      break;
+    case "lower":
+      if (currentNumber < prevNumber) {
+        console.log("Lesss goo");
+        showResult();
+        playerWon = true;
+      }
+      else if (currentNumber > prevNumber) {
+        console.log("Aww damn");
+        playerWon = false;
+        showResult();
+        score--;
+        displayScore();
+      }
+      else if (currentNumber == prevNumber) {
+        console.log("bruh");
+        playerWon = undefined;
+        showResult();
+      }
+      break;
+  }
+  playerBet = "nothing";
+}
+
+function imgChange() {
+  switch (currentNumber) {
+    case 1:
+      dice.src = "/images/1.png";
+      break;
+    case 2:
+      dice.src = "/images/2.png";
+      break;
+    case 3:
+      dice.src = "/images/3.png";
+      break;
+    case 4:
+      dice.src = "/images/4.png";
+      break;
+    case 5:
+      dice.src = "/images/5.png";
+      break;
+    case 6:
+      dice.src = "/images/6.png";
+      break;
+  }
+}
+
